4e236ac7b54db9db088991f09e6cd10a1ff23062
chpokerstyle <zoldaten48@gmail.com>
2023-03-17 11:44:24 +0300

    feat(validation): added validation of creating person (not nul fields)

--- src/main/java/com/volokitin/city/rest/models/CreatePersonRequest.java
+++ src/main/java/com/volokitin/city/rest/models/CreatePersonRequest.java
@@ -1,16 +1,21 @@
 package com.volokitin.city.rest.models;
 
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.lang.Nullable;
 
 import java.time.LocalDate;
 
 public record CreatePersonRequest(
 
+        @NotBlank(message = "Name is mandatory")
         String firstName,
+        @NotBlank(message = "Name is mandatory")
         String lastName,
+        @NotBlank(message = "Name is mandatory")
         short age,
         int series,
         int number,
+        @NotBlank(message = "Name is mandatory")
         LocalDate birthday,
         LocalDate dateCreatePassport,
         @Nullable
